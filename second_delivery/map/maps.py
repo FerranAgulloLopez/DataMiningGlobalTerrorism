6bb7d7d81eab55b74ac4d73f652fd18ced37ddad
Ferran Agulló López <fewrnap@gmail.com>
2019-10-02 21:49:58 +0200

    updates maps script with layers

--- second_delivery/map/maps.py
+++ second_delivery/map/maps.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import folium
 from folium.plugins import HeatMap
+from folium import FeatureGroup, LayerControl, Map, Marker
 
 data = pd.read_csv('../../first_delivery/datasets/preprocessed_dataset.csv')
 
@@ -11,18 +12,32 @@ latitude_longitude_democratic = data_democratic[['latitude','longitude']]
 
 hmap = folium.Map(location=[25, 24], zoom_start=3, )
 
-hm_wide = HeatMap( data = latitude_longitude_republican,
+democratic = FeatureGroup(name='Democratic')
+republican = FeatureGroup(name='Republican')
+
+HeatMap( data = latitude_longitude_republican,
                    min_opacity=0.2,
                    radius=6, blur=2,
                    gradient={.4: 'lime', .65: 'lime', 1: 'blue'},
                    max_zoom=1, 
-                 )
-hm_wide2 = HeatMap( data = latitude_longitude_democratic,
+                   layers='republican',
+                   overlay=False
+                 ).add_to(republican)
+HeatMap( data = latitude_longitude_democratic,
                    min_opacity=0.2,
                    radius=6, blur=2, 
                    gradient={.4: 'lime', .65: 'lime', 1: 'red'},
-                   max_zoom=1, 
-                 )
-hmap.add_child(hm_wide)
-hmap.add_child(hm_wide2)
-hmap.save('heatmap.html')
+                   max_zoom=1,
+                   layers='democratic',
+                   overlay=False
+                 ).add_to(democratic)
+
+m = Map(
+    location=[25, 24],
+    zoom_start=3,
+    tiles='Stamen Terrain'
+)
+democratic.add_to(m)
+republican.add_to(m)
+LayerControl().add_to(m)
+m.save('heatmap.html')
